9db122f7285b5dacbb42a18d32e5fc360c2c7534
KimSeHo <91kimseho91@gmail.com>
2019-10-15 17:16:56 +0900

    2019-10-15 17:16

--- getmoney/src/main/java/com/getmoney/web/CustomerController.java
+++ getmoney/src/main/java/com/getmoney/web/CustomerController.java
@@ -4,11 +4,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.getmoney.web.services.CustomerService;
+import com.getmoney.web.serviceimpls.CustomerServiceImpl;
 
 @Controller
+@RequestMapping("/customer/*")
 public class CustomerController {
 	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
-	@Autowired CustomerService customerService;
+	@Autowired CustomerServiceImpl customerService;
+	
+	@GetMapping("/count")
+	public String count(Model model) {
+		int count = customerService.countCustomers();
+		model.addAttribute("count",count);
+		return "home";
+	}
+	
+	@GetMapping("/info")
+	public String findCustomerId(@RequestParam("mid") String mid,
+			@RequestParam("mpw") String mpw) {
+		return "home";
+	}
 }
--- getmoney/src/main/java/com/getmoney/web/HomeController.java
+++ getmoney/src/main/java/com/getmoney/web/HomeController.java
@@ -6,8 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import com.getmoney.web.serviceimpls.CustomerServiceImpl;
 
 /**
@@ -20,7 +20,7 @@ public class HomeController {
 	
 	@Autowired CustomerServiceImpl customerService;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/")
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome  {}.", "HomeController");
 		int count = customerService.countCustomers();

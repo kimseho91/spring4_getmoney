a5c652eb4ad162303ffbc1476215c040d2665d11
KimSeHo <91kimseho91@gmail.com>
2019-10-17 18:33:17 +0900

    2019-10-17 18:33

--- getmoney/src/main/java/com/getmoney/web/CustomerController.java
+++ getmoney/src/main/java/com/getmoney/web/CustomerController.java
@@ -1,14 +1,20 @@
 package com.getmoney.web;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.getmoney.web.domains.CustomerDTO;
 import com.getmoney.web.serviceimpls.CustomerServiceImpl;
 
 @Controller
@@ -29,5 +35,14 @@ public class CustomerController {
 			@RequestParam("mpw") String mpw) {
 		return "index";
 		// test
+	}
+	@PostMapping("/join")
+	public @ResponseBody Map<?,?> join(@RequestBody CustomerDTO customer) {
+		logger.info("AJAX가 보낸 아이디 와 비번 {}",customer.getMid()+", "+customer.getMpw());
+		HashMap<String,String> map = new HashMap<>();
+		map.put("mid", customer.getMid());
+		map.put("mpw", customer.getMpw());
+		logger.info("map에 담긴 아이디 와 비번 {}",map.get("mid")+", "+map.get("mpw"));
+		return map;
 	}
 }
